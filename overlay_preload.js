93cce2f4fa85a089a7a841e64a30c9f7ff266a55
MrBlast98 <mrblast98@users.noreply.github.com>
2026-03-22 20:36:55 -0700

    Remove source files; keep app-only release artifacts

--- overlay_preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('overlayApi', {
-  onUpdate: (cb) => ipcRenderer.on('overlay-update', (e, args) => cb(args)),
-  getBounds: () => ipcRenderer.invoke('get-overlay-bounds'),
-  setBounds: (bounds) => ipcRenderer.send('set-overlay-bounds', bounds)
-});
